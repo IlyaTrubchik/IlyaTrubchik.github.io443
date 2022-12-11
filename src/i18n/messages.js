78be83fae2ab4739c54cd23db84ee0c365496cbe
Ilya Trubchik <ilya.thrubchik@mail.ru>
2022-12-11 15:20:10 +0300

    ReactLab2

---
+++ src/i18n/messages.js
@@ -0,0 +1,37 @@
+import { LOCALES } from "./locales"
+import { Doers_Eng } from "../JS/Scripts"
+import { Doers_ru } from "../JS/Scripts"
+export const messages={
+    
+    [LOCALES.ENGLISH]:{
+        'name':Doers_Eng[0].name,
+        'HomeBtn':"Main",
+        'ListBtn':"List of Figures",
+        'SiteInfo':"A site, or website, also a web site, is one or more logically interconnected web pages; also the location of the server content. Typically, a site on the Internet is an array of related data that has a unique address and is perceived by users as a single entity.",
+        'HumanOfDay':"Doer of the Day",
+        'time':"1978-Present",
+        'gotobtn':"Go",
+        'shortInfo':"Arrived on the 14th fall of 1978 in the city of Kazan, near the Vayskoy. In 1979, I moved to Belarus. In 2000, the Faculty of Management of the Belarussian Academy of Economics and Economics of the University passed away. In 2000, a number of young students were awarded the medal of the Ministry of Education and Science of the Russian Federation “For the best science practice” in the competition. In 2004, I received my doctorate, a prysudzhan scientific degree of a candidate of economic sciences.",
+        'Fedor':"Fedor Saprankov",
+        'Ilya':"Ilya Trubchik",
+        'Alex':"Alexey Manchik",
+        'Rulang':"Russian",
+        'Englang':"English",
+    },
+    [LOCALES.RUSSIAN]:{
+      'name':Doers_ru[0].name,
+      'HomeBtn':"Главная",
+      'ListBtn':"Список Деятелей",  
+      'SiteInfo':"Сайт, или веб-сайт, также веб-узел, — одна или несколько логически связанных между собой веб-страниц; также место расположения контента сервера. Обычно сайт в Интернете представляет собой массив связанных данных, имеющий уникальный адрес и воспринимаемый пользователями как единое целое.",
+      'HumanOfDay':"Деятель Дня",
+      'time':"1978-Настоящее время",
+      'gotobtn':"Перейти",
+      'shortInfo':"Нарадзіўся 14 лістапада 1978 года ў г. Казань, у сям’і вайскоўца. З 1979 года сям’я пераехала ў Беларусь. У 2000 годзе скончыў факультэт менеджменту Беларускага дзяржаўнага эканамічнага ўніверсітэта. У 2000 годзе ўзнагароджаны медалём Міністэрства адукацыі і навукі Расійскай Федэрацыі «За лепшую навуковую працу» сярод маладых вучоных па выніках адкрытага ўсерасійскага конкурсу. У 2004 годзе абараніў дысертацыю, прысуджана навуковая ступень кандыдата эканамічных навук.",
+      'Fedor':"Федор Сапраньков",
+      'Ilya':"Илья Трубчик",
+      'Alex':"Алексей Манчик",
+      
+        'Rulang':"Русский",
+        'Englang':"Английский",
+    }
+}
