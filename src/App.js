5b94a5ffab9cefcc7c3c3e4ccf8fb2275d816aca
Ilya Trubchik <ilya.thrubchik@mail.ru>
2022-12-13 23:01:02 +0300

    test3

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
         <Router>
         <Routes>
       <Route path="/"  exact element={<MainPage></MainPage>}/>
-      <Route path="/1" exact element={Person_Page(1)}/>
-      <Route path="/2" exact element={Person_Page(2)}/>
-      <Route path="/3" exact element={Person_Page(3)}/>
-      <Route path="/4" exact element={Person_Page(4)}/>
-      <Route path="/5" exact element={Person_Page(5)}/>
+      <Route path="/1" exact element={<Person_Page></Person_Page>}/>
+      <Route path="/2" exact element={<Person_Page></Person_Page>}/>
+      <Route path="/3" exact element={<Person_Page></Person_Page>}/>
+      <Route path="/4" exact element={<Person_Page></Person_Page>}/>
+      <Route path="/5" exact element={<Person_Page></Person_Page>}/>
       </Routes>
       </Router>
     )
