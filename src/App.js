1f8dd70c0d1e673e4d13f5a99e3aeb559b33947c
Ilya Trubchik <ilya.thrubchik@mail.ru>
2022-12-14 11:25:51 +0300

    afda

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
         <Router>
         <Routes>
       <Route path="/"  exact element={<MainPage></MainPage>}/>
-      <Route path="/1" exact element={<Person_Page></Person_Page>}/>
-      <Route path="/2" exact element={<Person_Page></Person_Page>}/>
-      <Route path="/3" exact element={<Person_Page></Person_Page>}/>
-      <Route path="/4" exact element={<Person_Page></Person_Page>}/>
-      <Route path="/5" exact element={<Person_Page></Person_Page>}/>
+      <Route path="https://ilyatrubchik.github.io/1" exact element={<Person_Page></Person_Page>}/>
+      <Route path="https://ilyatrubchik.github.io/2" exact element={<Person_Page></Person_Page>}/>
+      <Route path="https://ilyatrubchik.github.io/3" exact element={<Person_Page></Person_Page>}/>
+      <Route path="https://ilyatrubchik.github.io/4" exact element={<Person_Page></Person_Page>}/>
+      <Route path="https://ilyatrubchik.github.io/5" exact element={<Person_Page></Person_Page>}/>
       </Routes>
       </Router>
     )
